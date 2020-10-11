e9697dc80f80a66afe7074895f926e71d3e29996
jessegj <jen18055@byui.edu>
2020-10-10 21:03:15 -0600

    Week04 caddition

---
+++ Week04/hero.js
@@ -0,0 +1,39 @@
+const form = document.forms['hero'];
+form.addEventListener('submit', makeHero, false);
+
+function makeHero(event) {
+    event.preventDefault(); // prevent the form from being submitted
+    const hero = {}; // create an empty object
+    hero.name = form.heroName.value; // create a name property based on the input field's value
+    hero.city = form.city.value;
+    hero.origin = form.origin.value;
+    form.origin.value = 'Born as Kal-El on the planet Krypton...';
+
+    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
+
+    form.addEventListener('submit',validate,false);
+    return hero;
+
+const label = form.querySelector('label');
+const error = document.createElement('div');
+error.classList.add('error');
+error.textContent = '! Your name is not allowed to start with X.';
+label.append(error);
+
+function validate(event) {
+    const firstLetter = form.heroName.value[0];
+    if (firstLetter.toUpperCase() === 'X') {
+        event.preventDefault();
+        alert('Your name is not allowed to start with X!');
+    }
+}
+
+function validateInline() {
+    const heroName = this.value.toUpperCase();
+    if(heroName.startsWith('X')){
+    error.style.display = 'block';
+    } else {
+    error.style.display = 'none';
+    }
+}
+}
---
+++ Week04/main.js
@@ -0,0 +1,32 @@
+const input = form.elements.searchInput;
+input.addEventListener('focus', () => alert('focused'), false);
+
+input.addEventListener('blur', () => alert('blurred'), false);
+const form = document.forms['search'];
+
+form.addEventListener ('submit', search, false);
+function search() {
+    alert(' Form Submitted');
+}
+
+function search(event) {
+    alert('Form Submitted');
+    event.preventDefault();
+}
+
+function search(event) {
+    alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
+}
+
+input.value = 'Search Here';
+
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
+    }
+}, false);
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false);
