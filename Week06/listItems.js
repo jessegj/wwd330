5e0bf2d905633d532d0d546be63ec4e1be36a12b
jessegj <jen18055@byui.edu>
2020-10-23 21:05:26 -0600

    List added

--- Week06/listItems.js
+++ Week06/listItems.js
@@ -10,9 +10,39 @@
 function addItem(){
     var ul = document.getElementById("list");
     var itemList = document.getElementById("itemList");
-    var li = document.createElement("li");
-    li.setAttribute('id',itemList.value);
-    li.appendChild(document.createTextNode(itemList.value));
+    var li = document.createElement("input");
+    var itemCount = document.getElementById("itemCount");
+    var itemCountValue = itemCount.value;
+    var label = document.createElement("label");
+    var br = document.createElement("br");
+    itemCountValue++;
+    li.setAttribute("id", "item" + itemCountValue);
+    li.setAttribute("type", "checkbox");
+    li.setAttribute("name", "item" + itemCountValue);
+    li.setAttribute("checked", "false");
     ul.appendChild(li);
+    label.setAttribute("for", "item" + itemCountValue);
+    label.setAttribute("id", "itemFor" + itemCountValue);
+    label.innerHTML = itemList.value;
+    ul.appendChild(label);
+    ul.appendChild(br);
+    itemCount.value = itemCountValue;  
 
 }
+function deleteItem() {
+    var itemCount = document.getElementById("itemCount").value;
+    var itemCountRemoved = 0;
+    for (var index = 1; index <= itemCount; index++) {
+        var item = document.getElementById("item" + index);
+        if (item.checked) {
+            var label = document.getElementById("itemFor" + index);
+            label.remove();
+            item.remove();
+            itemCountRemoved++;
+
+        }    
+
+    }
+    document.getElementById("itemCount").value = itemCount - itemCountRemoved;
+}
+ 
